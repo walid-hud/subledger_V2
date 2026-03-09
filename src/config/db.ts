9112b2fd374d993bbb10262e599c9effb33f9cb8
walid-hud <walidthehud@gmail.com>
2026-03-09 15:23:52 +0000

    setup db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { wrap } from '../utils/try.js';
+import { da } from 'zod/locales';
 
 export const connectDB = async () => {
   const [data, error] = await wrap(mongoose.connect(process.env.MONGO_URI!));
@@ -7,5 +8,9 @@ export const connectDB = async () => {
     console.error(error);
     process.exit(1);
   }
+  data.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
   console.log('MongoDB connected');
 };
